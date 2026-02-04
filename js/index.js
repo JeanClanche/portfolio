15de2183fad54771db4df773afa107cc812048c7
Pedro <pedro@mail.com>
2026-02-04 11:49:41 +0100

    revamp

--- js/index.js
+++ js/index.js
@@ -4,19 +4,9 @@ const page = url[url.length -1]
 async function addNav() {
     const resp = await fetch("includes/navbar.html");
     const html = await resp.text();
-    document.getElementById('mainrow').insertAdjacentHTML("afterbegin", html);
+    document.body.insertAdjacentHTML("afterbegin", html);
 }
 
 document.addEventListener("DOMContentLoaded", async function() {//quand la page est chargée
-    await addNav()
-    let element
-    switch(page){
-        case "":
-            elements = ""
-            break
-        default:
-            element = ""
-
-    }
-    document.getElementById(element).classList.add("active")
+    addNav()
 })
